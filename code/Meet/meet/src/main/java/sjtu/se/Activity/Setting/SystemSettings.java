699097e75f664935540fdba7ae4ff17585a3be35
hull <hulili0203@126.com>
2015-12-07 14:52:23 +0800

    系统设置界面

--- code/Meet/meet/src/main/java/sjtu/se/Activity/Setting/SystemSettings.java
+++ code/Meet/meet/src/main/java/sjtu/se/Activity/Setting/SystemSettings.java
@@ -3,14 +3,12 @@ package sjtu.se.Activity.Setting;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.KeyEvent;
 import sjtu.se.Fragment.Pref_Fragment;
 import sjtu.se.Meet.R;
 
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.content.IntentFilter;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -18,10 +16,6 @@ import sjtu.se.Activity.ActivityControlCenter;
 
 public class SystemSettings extends AppCompatActivity {
 
-	//public static boolean dayOrNightChange = false;
-	//public static boolean night = false;
-
-	private IntentFilter intentFilter;
 	private BroadcastReceiver receiver = new BroadcastReceiver() {
 		@Override
 		public void onReceive(Context context, Intent intent) {
@@ -33,56 +27,39 @@ public class SystemSettings extends AppCompatActivity {
 	};
 
 	@Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pref);
 
-	protected void onCreate(Bundle savedInstanceState) {
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+        ActionBar ab = getSupportActionBar();
+        ab.setDisplayHomeAsUpEnabled(true);
 
-		//SystemSettings.dayOrNight(this);
+        getFragmentManager().beginTransaction().replace(R.id.pref_fragment_container, new Pref_Fragment()).commit();
+    }
 
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_pref);
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.search, menu);
+        return true;
+    }
 
-		Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
-		setSupportActionBar(myToolbar);
-		ActionBar ab = getSupportActionBar();
-		ab.setDisplayHomeAsUpEnabled(true);
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
 
-		getFragmentManager().beginTransaction().replace(R.id.pref_fragment_container, new Pref_Fragment()).commit();
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            //this.startActivity(new Intent(SystemSettings.this, SystemSettings.class));
+            return false;
+        }
 
-		//intentFilter = new IntentFilter();
-		//intentFilter.addAction(ActivityControlCenter.ACTIVITY_EXIT_ACTION);
-		//this.registerReceiver(receiver, intentFilter);
-
-	}
-
-	@Override
-	public boolean onKeyDown(int keyCode, KeyEvent event)  {
-		if (keyCode == KeyEvent.KEYCODE_MENU) {
-			return true;
-		}
-		return super.onKeyDown(keyCode, event);
-	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		//getMenuInflater().inflate(R.menu.search, menu);
-		return true;
-	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-
-		//noinspection SimplifiableIfStatement
-		if (id == R.id.action_settings) {
-			//this.startActivity(new Intent(SystemSettings.this, SystemSettings.class));
-			return false;
-		}
-
-		return super.onOptionsItemSelected(item);
-	}
+        return super.onOptionsItemSelected(item);
+    }
 
 }
